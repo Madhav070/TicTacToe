6e71b855179600edbf56987d34d59b81905c0d76
Madhav070 <114184904+Madhav070@users.noreply.github.com>
2024-07-12 01:10:22 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,74 +1,86 @@
-let currPlayer="X";
-let btn=document.getElementsByClassName('choiceBox');
-for(let element of btn){
-  element.addEventListener('click',handleClick);
+let currPlayer = "X";
+let btn = document.getElementsByClassName("choiceBox");
+for (let element of btn) {
+  element.addEventListener("click", handleClick);
 }
 
-let count=0;
+let count = 0;
 
-function handleClick(){
-  if(currPlayer=="X"){
-    this.innerHTML=`<p class="text-red-500">${currPlayer}</p>`;
+function handleClick() {
+  if (currPlayer == "X") {
+    this.innerHTML = `<p class="text-red-500">${currPlayer}</p>`;
+  } else {
+    this.innerHTML = `<p class="text-blue-500">${currPlayer}</p>`;
   }
-  else{
-    this.innerHTML=`<p class="text-blue-500">${currPlayer}</p>`;
+  count++;
+  setTimeout(checkCondition, 300);
+}
+
+function checkCondition() {
+  if (checkWinner()) {
+    alert(`${currPlayer} is the Winner.`);
+    clearBoard();
+    count = 0;
+  } else if (count == 9) {
+    alert("It is a Draw!");
+    clearBoard();
+    count = 0;
+  } else {
+    if (currPlayer == "X") {
+      currPlayer = "O";
+    } else if (currPlayer == "O") {
+      currPlayer = "X";
+    }
   }
-  count++
-  setTimeout(checkCondition,300);
 }
 
-function checkCondition(){
-    if(checkWinner()){
-      alert(`${currPlayer} is the Winner.`);
-      clearBoard();
-      count=0;
-    }
-    else if(count==9){
-        alert('It is a Draw!');
-        clearBoard();
-        count=0;
-    }
-    else{
-      if(currPlayer=="X"){
-        currPlayer="O";
-      }
-      else if(currPlayer=="O"){
-        currPlayer="X";
-      }
-    }
-}
-
-function checkWinner(){
-  for(let i=0;i<=6;i=i+3){
-    if(btn[i].textContent==btn[i+1].textContent && btn[i+1].textContent==btn[i+2].textContent && (btn[i].textContent=="X" || btn[i].textContent=="O")){
+function checkWinner() {
+  for (let i = 0; i <= 6; i = i + 3) {
+    if (
+      btn[i].textContent == btn[i + 1].textContent &&
+      btn[i + 1].textContent == btn[i + 2].textContent &&
+      (btn[i].textContent == "X" || btn[i].textContent == "O")
+    ) {
       return true;
     }
   }
-  for(let i=0;i<3;i++){
-    if(btn[i].textContent==btn[i+3].textContent && btn[i+3].textContent==btn[i+6].textContent && (btn[i].textContent=="X" || btn[i].textContent=="O")){
+  for (let i = 0; i < 3; i++) {
+    if (
+      btn[i].textContent == btn[i + 3].textContent &&
+      btn[i + 3].textContent == btn[i + 6].textContent &&
+      (btn[i].textContent == "X" || btn[i].textContent == "O")
+    ) {
       return true;
     }
   }
-  if(btn[0].textContent==btn[4].textContent && btn[4].textContent==btn[8].textContent && (btn[0].textContent=="X" || btn[0].textContent=="O")){
+  if (
+    btn[0].textContent == btn[4].textContent &&
+    btn[4].textContent == btn[8].textContent &&
+    (btn[0].textContent == "X" || btn[0].textContent == "O")
+  ) {
     return true;
   }
-  if(btn[2].textContent==btn[4].textContent && btn[4].textContent==btn[6].textContent && (btn[2].textContent=="X" || btn[2].textContent=="O")){
+  if (
+    btn[2].textContent == btn[4].textContent &&
+    btn[4].textContent == btn[6].textContent &&
+    (btn[2].textContent == "X" || btn[2].textContent == "O")
+  ) {
     return true;
   }
   return false;
 }
 
-function clearBoard(){
-  for(let element of btn){
-    element.textContent="";
+function clearBoard() {
+  for (let element of btn) {
+    element.textContent = "";
   }
 }
 
-function addColor(currBtn){
-  if(currPlayer=="X"){
-    currBtn.classList.add('text-red-500');
+function addColor(currBtn) {
+  if (currPlayer == "X") {
+    currBtn.classList.add("text-red-500");
   }
-  if(currPlayer=="O"){
-    currBtn.classList.add('text-blue-500');
+  if (currPlayer == "O") {
+    currBtn.classList.add("text-blue-500");
   }
 }
